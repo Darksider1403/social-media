a1187449aa9192aa3783740c670658c273206ed2
Darksider1403 <21130489@st.hcmuaf.edu.vn>
2025-04-21 13:35:57 +0700

    update FE: create getALlPost

--- social-media-be/src/main/java/com/java/social_media/controller/StoryController.java
+++ social-media-be/src/main/java/com/java/social_media/controller/StoryController.java
@@ -1,5 +1,6 @@
 package com.java.social_media.controller;
 
+import com.java.social_media.exceptions.UserException;
 import com.java.social_media.models.Story;
 import com.java.social_media.models.User;
 import com.java.social_media.service.StoryService;
@@ -28,6 +29,14 @@ public class StoryController {
     public List<Story> findUsersStory(@PathVariable Integer userId) throws Exception {
         List<Story> stories = storyService.findStoryByUserId(userId);
 
+        return stories;
+    }
+
+    @GetMapping("/api/story/feed")
+    public List<Story> getStoryFeed(@RequestHeader("Authorization") String jwt) throws UserException {
+        User reqUser = userService.findUserByJwt(jwt);
+        // Get stories from users that the current user follows
+        List<Story> stories = storyService.findStoryFeed(reqUser.getId());
         return stories;
     }
 }
--- social-media-be/src/main/java/com/java/social_media/service/StoryService.java
+++ social-media-be/src/main/java/com/java/social_media/service/StoryService.java
@@ -1,5 +1,6 @@
 package com.java.social_media.service;
 
+import com.java.social_media.exceptions.UserException;
 import com.java.social_media.models.Story;
 import com.java.social_media.models.User;
 
@@ -9,4 +10,6 @@ public interface StoryService {
     Story createStory(Story story, User user);
 
     List<Story> findStoryByUserId(Integer userId) throws Exception;
+
+    List<Story> findStoryFeed(Integer userId) throws UserException;
 }
